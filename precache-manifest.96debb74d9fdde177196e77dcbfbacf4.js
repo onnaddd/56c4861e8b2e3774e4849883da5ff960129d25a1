47597326cc6f0944ba05535b33306fd228742efb
dioandita <dioonad@gmail.com>
2020-01-22 16:48:20 +0700

    Updates

---
+++ precache-manifest.96debb74d9fdde177196e77dcbfbacf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc180969adfa97bac1178b1a4b8e44b6",
+    "url": "/56c4861e8b2e3774e4849883da5ff960129d25a1/index.html"
+  },
+  {
+    "revision": "ff0eaca63e10fd08d659",
+    "url": "/56c4861e8b2e3774e4849883da5ff960129d25a1/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "524f4e203f2632bde88f",
+    "url": "/56c4861e8b2e3774e4849883da5ff960129d25a1/static/js/2.f2d2f7ce.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/56c4861e8b2e3774e4849883da5ff960129d25a1/static/js/2.f2d2f7ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff0eaca63e10fd08d659",
+    "url": "/56c4861e8b2e3774e4849883da5ff960129d25a1/static/js/main.57362abd.chunk.js"
+  },
+  {
+    "revision": "c875241b8189a61817b7",
+    "url": "/56c4861e8b2e3774e4849883da5ff960129d25a1/static/js/runtime-main.6df87aac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/56c4861e8b2e3774e4849883da5ff960129d25a1/static/media/logo.5d5d9eef.svg"
+  }
+]);
